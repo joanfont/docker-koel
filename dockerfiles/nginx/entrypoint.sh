2614a87a85649189bca8c9dc6e02270ac120d1e5
Joan Font <joanfont@gmail.com>
2016-01-20 20:47:57 +0100

    keep trying

--- dockerfiles/nginx/entrypoint.sh
+++ dockerfiles/nginx/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# replace variables for upstream's ips and ports
+cp -r /etc/nginx/sites-available /etc/nginx/sites-enabled
+
 sed -i "s/%fpm-ip%/$PHP_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
 sed -i "s/%fpm-port%/$PHP_PORT_9000_TCP_PORT/" /etc/nginx/nginx.conf
 
+chown -R www-data:www-data /app
 nginx -g "daemon off;"
